5e1f01e7d65d4fdb1ea4b1d726e0819ac036fc4a
xujian <2513573453@qq.com>
2021-07-18 10:39:44 +0800

    feat:修改路由

--- routes/admin.js
+++ routes/admin.js
@@ -3,13 +3,13 @@ const router=express.Router()
 //引入自定义的controller
 const AdminController=require('../controllers/admin')
 //管理员列表
-// router.get('/list',AdminController.list)
+router.get('/list',AdminController.list)
 //查询管理员详情
-// router.get('/:id',AdminController.info)
+router.get('/:id',AdminController.info)
 //新增管理员
-// router.post('/add',AdminController.add)
+router.post('/add',AdminController.add)
 //修改管理员
-// router.put('/edit',AdminController.update)
+router.put('/edit',AdminController.update)
 //删除管理员
-// router.delete('/delete',AdminController.remove)
+router.delete('/delete',AdminController.remove)
 module.exports=router
